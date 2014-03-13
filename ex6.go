ff837091111813648c198329a32689c0c3f63ba2
sjokolAda <freworr@gmail.com>
2014-03-13 12:31:19 +0100

    master/slave X

--- ex6.go
+++ ex6.go
@@ -1,52 +1,70 @@
 package main
+
 //https://groups.google.com/forum/#!topic/golang-china/KG0Bgf0CAQc
 import (
 	"fmt"
-	."net"
-	"time"
-	"strconv"
+	. "net"
 	"os/exec"
+	"strconv"
+	"time"
 )
+
 const (
 	MAXWAIT = time.Second
 )
 
-
 func main() {
 	nr := backup()
-	cmd, _ := exec.Command("mate-terminal").Output()
-	// spawn new window
-	//	mate terminal
+
+	cmd := exec.Command("mate-terminal", "-x", "go", "run", "ex6.go")
+	cmd.Start()
+
 	primary(nr)
-	cmd.exec.Command("go run ex6.go").Output()
+
 }
 
 func backup() int {
+	//fmt.Println("i have backup")
 	var nr int
 	buf := make([]byte, 1024)
-	address, err := ResolveUDPAddr("udp", "129.241.187.255:20020")
-	conn, _ := ListenUDP("udp", address)
-
+	address, err := ResolveUDPAddr("udp", ":20019") //leser bare fra porten generellt
+	conn, err := ListenUDP("udp", address)
+	
+	if err != nil {
+		fmt.Println("backup conn create error:", err)
+	}
+	
+	fmt.Println("backup")
 	for {
-		conn.SetReadDeadline(time.Now().Add(1*time.Second))
-		n, _, _:= conn.ReadFromUDP(buf) //n contanis numbers of used bytes
-		if err == nil {		
-			nr,_ = strconv.Atoi(string(buf[0:n]))
+		
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		n, _, err := conn.ReadFromUDP(buf) //n contanis numbers of used bytes
+		if err == nil {
+			nr, _ = strconv.Atoi(string(buf[0:n]))
+			//fmt.Println("nr: ", nr)
 		} else {
-			break;
+			break
 		}
 	}
 	conn.Close()
-	return nr;
+	return nr
 }
 
-func primary(nr int){
-		address, _ := ResolveUDPAddr("udp", "129.241.187.255:20020")
-		conn, _ := DialUDP("udp", address, nil)
-		for {
-			nr++
-			fmt.Println(nr)
-			conn.Write([]byte(strconv.Itoa(nr)))
-			time.Sleep(300*time.Millisecond)
+func primary(nr int) {
+	address, _ := ResolveUDPAddr("udp", "129.241.187.255:20019")
+	conn, _ := DialUDP("udp", nil, address)
+	
+	//fmt.Println(a)
+	fmt.Println("primary")
+	for {
+		
+		nr++
+		fmt.Println(nr)
+
+		_, err := conn.Write([]byte(strconv.Itoa(nr)))
+		if err != nil {
+			fmt.Println("fail")
 		}
+		time.Sleep(2000 * time.Millisecond)
+	}
 }
--- testofgithub.go
+++ testofgithub.go
@@ -1,52 +1,82 @@
 package main
+
 //https://groups.google.com/forum/#!topic/golang-china/KG0Bgf0CAQc
 import (
 	"fmt"
-	."net"
-	"time"
-	"strconv"
+	. "net"
 	"os/exec"
+	"strconv"
+	"time"
 )
+
 const (
 	MAXWAIT = time.Second
 )
 
-
 func main() {
 	nr := backup()
-	cmd, _ := exec.Command("mate-terminal").Output()
+	/*
+		//_ = exec.Command("go run testofgithub.go")
+		cmd := exec.Command("go run testofgithub.go")
+		err := cmd.Start()
+		if err != nil {
+			fmt.Println("err")
+		}
+		err = cmd.Wait()
+	*/
+	cmd := exec.Command("mate-terminal", "-x", "go", "run", "testofgithub.go")
+	cmd.Start()
+
+	primary(nr)
+
 	// spawn new window
 	//	mate terminal
-	primary(nr)
-	cmd.exec.Command("go run ex6.go").Output()
+
+	//cmd.Output("go run testofgithub.go")
+
 }
 
 func backup() int {
+	fmt.Println("i have backup")
 	var nr int
 	buf := make([]byte, 1024)
-	address, err := ResolveUDPAddr("udp", "129.241.187.255:20020")
-	conn, _ := ListenUDP("udp", address)
+	address, err := ResolveUDPAddr("udp", ":20019")
+	conn, err := ListenUDP("udp", address)
+	fmt.Println("backup conn create error:", err)
 
 	for {
-		conn.SetReadDeadline(time.Now().Add(1*time.Second))
-		n, _, _:= conn.ReadFromUDP(buf) //n contanis numbers of used bytes
-		if err == nil {		
-			nr,_ = strconv.Atoi(string(buf[0:n]))
+		fmt.Println("backup")
+		conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+		n, _, err := conn.ReadFromUDP(buf) //n contanis numbers of used bytes
+		if err == nil {
+			fmt.Println("h")
+			fmt.Println("buf: ", string(buf[0:n]))
+			nr, _ = strconv.Atoi(string(buf[0:n]))
+			fmt.Println("nr: ", nr)
 		} else {
-			break;
+			fmt.Println("t")
+			break
 		}
 	}
 	conn.Close()
-	return nr;
+	return nr
 }
 
-func primary(nr int){
-		address, _ := ResolveUDPAddr("udp", "129.241.187.255:20020")
-		conn, _ := DialUDP("udp", address, nil)
-		for {
-			nr++
-			fmt.Println(nr)
-			conn.Write([]byte(strconv.Itoa(nr)))
-			time.Sleep(300*time.Millisecond)
+func primary(nr int) {
+	address, _ := ResolveUDPAddr("udp", "129.241.187.255:20019")
+	conn, _ := DialUDP("udp", nil, address)
+	var a []byte
+	fmt.Println(a)
+	for {
+		fmt.Println("primary")
+		nr++
+		fmt.Println(nr)
+
+		a = []byte(strconv.Itoa(nr))
+		_, err := conn.Write(a)
+		if err != nil {
+			fmt.Println("fail")
 		}
+		time.Sleep(300 * time.Millisecond)
+	}
 }
