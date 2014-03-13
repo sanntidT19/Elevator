0385c31b3ebcf0a2cc684595a840f15d6f1e1d79
sanntidT19 <yngveska@stud.ntnu.no>
2014-03-13 08:16:21 +0100

    latest

--- communication.go
+++ communication.go
@@ -9,21 +9,28 @@ type elevatorNr struct {
 	ip string
 }
 
-func getMessage() {
+func getMessage(networkChan <- chan []byte]) { //gets chan from network module
 	for {
-		message := networkChan.Incomming
-		go extractMessage(message)
+		message := <-networkChan
+		extractMessage(message)
 	}
 }
+func packMessage(outgoingChan chan string, networkChan chan []byte) {
+	tempMessage := <- outgoingChan
+	codedMessage := json.Marshall(tempMessage)
+	networkChan <- codedMessage
+}
 
-func extractMessage(message network.Message) {}
-	tempMessage := message.IncommingMessage
-	tempMessage = json.Decode(tempMessage)
-	if hasPrefix(tempMessage) == "ord" {
-		//remove prefix and run order
+
+func extractMessage(messageB []byte, incomingChan chan string) {}
+
+	decodedMessage := json.Decode(messageB)
+	if HasPrefix(decodedMessage) == "ord" {
+		decodedMessage = TrimPrefix(decodedMessage, "ord") //remove prefix and run order
+	}
+	else if HasPrefix(decodedMessage) == "iah" { //if timeLimit has run out -> call chooseMaster
+		decodedMessage = TrimPrefix(decodedMessage, "iah")
 
 	}
-	else if hasPrefix(tempMessage) == "iah" {
-		//if timeLimit has run out -> call chooseMaster
-	}
+	incomingChan <- decodedMessage
 }
